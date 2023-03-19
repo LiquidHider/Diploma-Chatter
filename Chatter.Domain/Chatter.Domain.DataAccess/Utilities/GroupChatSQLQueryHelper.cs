d94c5809c4aa342158fe33f14cac5b6ba7fc96f6
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-03-19 21:21:08 +0300

    Add GroupChatSQLQueryHelper.

---
+++ Chatter.Domain/Chatter.Domain.DataAccess/Utilities/GroupChatSQLQueryHelper.cs
@@ -0,0 +1,34 @@
+﻿using Chatter.Domain.DataAccess.Models;
+using Dapper;
+
+namespace Chatter.Domain.DataAccess.Utilities
+{
+    internal class GroupChatSQLQueryHelper : SQLQueryHelper
+    {
+        public const string CreateQuery = @"
+           INSERT INTO [dbo].[GroupChats] ([ID],[Name],[Description]) 
+            VALUES(@ID, @Name, @Description)";
+
+        public const string DeleteQuery = @"
+        DELETE FROM [dbo].[GroupChats]
+        WHERE [ID] = @ID";
+
+        public const string GetOneQuery = @"
+            SELECT TOP (1) *
+            FROM [chatter].[dbo].[GroupChats]
+            {0}";
+
+
+        public const string ListQuery = @"
+            SELECT *
+            FROM [dbo].[GroupChats] 
+            {0}
+            {1}";
+
+        public const string UpdateQuery = @"
+            UPDATE [dbo].[GroupChats] 
+            SET {0}
+            WHERE ID = @ID";
+
+    }
+}
