fcc6870e6e1da3d8ca0c616d31fe90ead4ad1c96
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-03-19 20:31:31 +0300

    Change to update model.

---
+++ Chatter.Domain/Chatter.Domain.DataAccess/Models/UpdateGroupChat.cs
@@ -0,0 +1,11 @@
+﻿namespace Chatter.Domain.DataAccess.Models
+{
+    internal class UpdateGroupChat
+    {
+        public Guid ID { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
