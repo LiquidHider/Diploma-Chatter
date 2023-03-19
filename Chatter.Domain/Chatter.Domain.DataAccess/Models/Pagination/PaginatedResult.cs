625b4a7d59e6a10d8caf75afe4bbf75206fc22a9
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-03-19 13:48:03 +0300

    Add PaginatedResult.

---
+++ Chatter.Domain/Chatter.Domain.DataAccess/Models/Pagination/PaginatedResult.cs
@@ -0,0 +1,22 @@
+﻿using Chatter.Domain.Common.Enums;
+using Chatter.Domain.Common.Interfaces;
+
+namespace Chatter.Domain.DataAccess.Models.Pagination
+{
+    public class PaginatedResult<TEntity, TSort> : IPaginationResult<TSort> where TSort : Enum
+    {
+        public int TotalCount { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int PageNumber { get; set; }
+
+        public int PageSize { get; set; }
+
+        public SortOrder SortOrder { get; set; }
+
+        public TSort SortBy { get; set; }
+
+        public IList<TEntity>? Entities { get; set; }
+    }
+}
