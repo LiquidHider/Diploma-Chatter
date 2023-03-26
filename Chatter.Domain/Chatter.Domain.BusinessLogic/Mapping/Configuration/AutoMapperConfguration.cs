4eb67b3aaea44b65995bdcfb097ec0c2c3aba3ff
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-03-26 23:21:33 +0300

    Add AutoMapper.

---
+++ Chatter.Domain/Chatter.Domain.BusinessLogic/Mapping/Configuration/AutoMapperConfguration.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Chatter.Domain.BusinessLogic.Mapping.Profiles;
+using Chatter.Domain.DataAccess.Interfaces;
+
+namespace Chatter.Domain.BusinessLogic.Mapping.Configuration
+{
+    internal class AutoMapperConfguration
+    {
+        public MapperConfiguration Configure()
+        {
+            MappingProfile profile = new();
+            return new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(profile);
+            });
+        }
+    }
+}
---
+++ Chatter.Domain/Chatter.Domain.BusinessLogic/Mapping/Profiles/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Chatter.Domain.BusinessLogic.Models;
+using Chatter.Domain.DataAccess.Models;
+
+namespace Chatter.Domain.BusinessLogic.Mapping.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+
+            CreateMap<Report, ReportModel>()
+                .ForMember(dest => dest.ReportedUserID, opt => opt.MapFrom(src => src.ReportedUser.ID));
+
+            CreateMap<ChatUser, ChatUserModel>();
+        }
+    }
+
+}
