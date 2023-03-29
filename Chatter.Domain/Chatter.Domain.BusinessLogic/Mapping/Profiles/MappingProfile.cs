a09dca65075ee34b0863f42090f2b74ab86ed53c
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-03-29 10:54:11 +0300

    Add new mappings.

--- Chatter.Domain/Chatter.Domain.BusinessLogic/Mapping/Profiles/MappingProfile.cs
+++ Chatter.Domain/Chatter.Domain.BusinessLogic/Mapping/Profiles/MappingProfile.cs
@@ -1,5 +1,7 @@
 ﻿using AutoMapper;
 using Chatter.Domain.BusinessLogic.Models;
+using Chatter.Domain.BusinessLogic.Models.ChatMessages;
+using Chatter.Domain.BusinessLogic.Models.Update;
 using Chatter.Domain.DataAccess.Models;
 
 namespace Chatter.Domain.BusinessLogic.Mapping.Profiles
@@ -8,11 +10,14 @@ namespace Chatter.Domain.BusinessLogic.Mapping.Profiles
     {
         public MappingProfile()
         {
-
             CreateMap<Report, ReportModel>();
             CreateMap<ReportModel, Report>();
-
             CreateMap<ChatUser, ChatUserModel>();
+            CreateMap<UpdateMessage, UpdateChatMessageModel>();
+            CreateMap<PrivateChatMessage, ChatMessageModel>()
+                .ForMember(dest => dest.RecipientUser, opt => opt.MapFrom(src => src.Recipient));
+            CreateMap<GroupChatMessage, ChatMessageModel>()
+              .ForMember(dest => dest.RecipientGroup, opt => opt.MapFrom(src => src.Recipient));
         }
     }
 
