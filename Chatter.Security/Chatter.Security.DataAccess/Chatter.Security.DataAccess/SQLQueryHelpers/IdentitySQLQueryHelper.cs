f6e600b7f55dc0dd1924576e1a63f147fc369b32
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-02 19:34:21 +0300

    Add SQL query helpers.

---
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.DataAccess/SQLQueryHelpers/IdentitySQLQueryHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Chatter.Security.DataAccess.SQLQueryHelpers
+{
+    internal class IdentitySQLQueryHelper : SQLQueryHelper
+    {
+        public const string CreateQuery = @"
+        INSERT INTO [dbo].[Identities] ([ID], [Email], [UserTag], [PasswordHash], [PasswordKey], [UserID])
+        VALUES (@ID, @Email, @UserTag, @PasswordHash, @PasswordKey, @UserID)";
+
+        public const string DeleteQuery = @"DELETE FROM [dbo].[Identities] WHERE [ID] = @ID";
+
+        public const string GetOneQuery = @"
+        SELECT TOP (1) *
+        FROM [dbo].[Identities]
+        WHERE [ID] = @ID";
+
+        public const string UpdateQuery = @"
+        UPDATE [dbo].[Identities]
+        SET {0}
+        WHERE [ID] = @ID";
+    }
+}
---
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.DataAccess/SQLQueryHelpers/SQLQueryHelper.cs
@@ -0,0 +1,53 @@
+﻿using Dapper;
+using System.Reflection;
+using System.Text;
+
+namespace Chatter.Security.DataAccess.SQLQueryHelpers
+{
+    internal abstract class SQLQueryHelper
+    {
+        public string CreateQueryUpdateParameters<TModel>(TModel model, DynamicParameters parameters)
+        {
+            PropertyInfo[] modelProperties = model.GetType().GetProperties().Where(x => x.GetValue(model) != null).ToArray();
+            var builder = new StringBuilder();
+            foreach (PropertyInfo modelProperty in modelProperties)
+            {
+                var propertyValue = modelProperty.GetValue(model);
+                bool isPropertyLast = modelProperty.Name == modelProperties.Last().Name;
+                if (propertyValue != null && modelProperty.Name.ToUpper() != "ID")
+                {
+                    var updateQueryPart = $"[{modelProperty.Name}] = @{modelProperty.Name}" + (isPropertyLast ? string.Empty : ",");
+                    builder.AppendLine(updateQueryPart);
+                    parameters.Add($"@{modelProperty.Name}", propertyValue);
+                }
+            }
+            return builder.ToString();
+        }
+
+        public string Where(params string[] queryParts)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("WHERE ");
+            builder.AppendLine(queryParts.First());
+            foreach (var part in queryParts.Where(x => x != queryParts.First()))
+            {
+                builder.AppendLine($"OR {part}");
+            }
+
+            return builder.ToString();
+        }
+
+        public string OrderBy(string order, params string[] columns)
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("ORDER BY ");
+            foreach (var part in columns.Where(x => x != columns.Last()))
+            {
+                builder.AppendLine($"{part},");
+            }
+            builder.AppendLine(columns.Last());
+            builder.AppendLine(order);
+            return builder.ToString();
+        }
+    }
+}
