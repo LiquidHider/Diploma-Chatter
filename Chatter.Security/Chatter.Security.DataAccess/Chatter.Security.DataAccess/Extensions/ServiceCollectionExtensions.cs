9d9152d05f79ac3f6ad640c7199da2cbe0c8479b
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-03 14:47:07 +0300

    Add ServiceCollectionExtensions (Security.DataAccess)

---
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.DataAccess/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Chatter.Security.DataAccess.DbOptions;
+using Chatter.Security.DataAccess.Interfaces;
+using Chatter.Security.DataAccess.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chatter.Security.DataAccess.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddDataAccess(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<DatabaseOptions>(options => {
+                var connectionString = configuration?
+                .GetSection(DatabaseOptions.ConfigurationSectionName)
+                .GetSection(DatabaseOptions.ConnectionStringSectionName).Value;
+                options.ChatterDbConnection = connectionString;
+            });
+
+            services.AddTransient<IIdentityRepository, IdentityRepository>();
+
+            return services;
+        }
+    }
+}
