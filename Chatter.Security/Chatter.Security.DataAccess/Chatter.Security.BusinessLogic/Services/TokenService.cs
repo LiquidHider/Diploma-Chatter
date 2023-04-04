851e37a68ff46c916d12579996eb9969c0f0f4c3
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-04 23:32:52 +0300

    Add tests (TokenService). Fix TokenService.

--- Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.BusinessLogic/Services/TokenService.cs
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.BusinessLogic/Services/TokenService.cs
@@ -1,6 +1,7 @@
 ﻿using Chatter.Security.Core.Interfaces;
 using Chatter.Security.Core.Models;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
@@ -11,21 +12,34 @@ namespace Chatter.Security.Core.Services
     public class TokenService : ITokenService
     {
         private const int TOKEN_EXPIRATION_DATE_HOURS = 7;
-        private readonly SymmetricSecurityKey key;
+        private readonly SymmetricSecurityKey key; 
+        private readonly ILogger<TokenService> _logger;
         private readonly IIdentityService _identityService;
 
-        public TokenService(IConfiguration config, IIdentityService identityService)
+        public TokenService(IConfiguration config, IIdentityService identityService, ILogger<TokenService> logger)
         {
             key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+            _logger = logger;
             _identityService = identityService;
         }
+
         public async Task<string> CreateTokenAsync(Identity identity, CancellationToken cancellationToken)
         {
+            _logger.LogInformation("CreateTokenAsync : {@Details}", new { Class = nameof(TokenService), Method = nameof(CreateTokenAsync) });
+
             var claims = new List<Claim>()
             {
                 new Claim(JwtRegisteredClaimNames.NameId, identity.ID.ToString()),
                 new Claim(JwtRegisteredClaimNames.UniqueName, identity.UserTag),
             };
+
+            var identityFromDb = await _identityService.FindByIdAsync(identity.ID, cancellationToken);
+
+            if (identityFromDb == null) 
+            {
+                _logger.LogInformation("Identity doest not exist. {@Details}", new { IdentityID = identity.ID });
+                return string.Empty;
+            }
 
             var serviceResult = await _identityService.GetRolesAsync(identity.ID, cancellationToken);
 
---
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.Tests.UnitTests/TokenServiceTests.cs
@@ -0,0 +1,71 @@
+﻿using AutoFixture;
+using Chatter.Security.Core.Extensions;
+using Chatter.Security.Core.Interfaces;
+using Chatter.Security.Core.Models;
+using Chatter.Security.Core.Services;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Chatter.Security.Tests.UnitTests
+{
+    public class TokenServiceTests
+    {
+        private readonly TokenService _tokenService;
+        private readonly Fixture _fixture;
+        private readonly Mock<ILogger<TokenService>> _loggerMock;
+        private readonly Mock<IConfiguration> _configurationMock;
+        private readonly Mock<IIdentityService> _identityServiceMock;
+
+        public TokenServiceTests()
+        {
+            _fixture = new Fixture();
+
+            _loggerMock = new Mock<ILogger<TokenService>>();
+            _configurationMock = new Mock<IConfiguration>();
+            _configurationMock.Setup(x => x["TokenKey"]).Returns(_fixture.Create<string>());
+            _identityServiceMock = new Mock<IIdentityService>();
+
+            _tokenService = new TokenService(_configurationMock.Object, _identityServiceMock.Object, _loggerMock.Object);
+        }
+
+        [Fact]
+        public async void CreateTokenAsync_CreateTokenForExistingIdentity_ReturnsToken() 
+        {
+            //Arrange
+            CancellationToken token = default;
+            var identity = _fixture.Create<Identity>();
+            var valueServiceResultIdentity = new ValueServiceResult<Identity>().WithValue(identity);
+           
+            _identityServiceMock.Setup(x => x.FindByIdAsync(It.IsAny<Guid>(), token))
+                .Returns(Task.FromResult(valueServiceResultIdentity));
+            var userRoles = _fixture.Create<IList<string>>();
+
+            var valueServiceResultUserRoles = new ValueServiceResult<IList<string>>().WithValue(userRoles);
+            _identityServiceMock.Setup(x => x.GetRolesAsync(It.IsAny<Guid>(), token))
+                .Returns(Task.FromResult(valueServiceResultUserRoles));
+
+            //Act
+            var actual = await _tokenService.CreateTokenAsync(identity, token);
+
+            //Assert
+            actual.Should().NotBeEmpty();
+
+        }
+
+        [Fact]
+        public async void CreateTokenAsync_CreateTokenForInexistentIdentity_ReturnsEmptyString()
+        {
+            //Arrange
+            CancellationToken token = default;
+            var identity = _fixture.Create<Identity>();
+
+            //Act
+            var actual = await _tokenService.CreateTokenAsync(identity, token);
+
+            //Assert
+            actual.Should().BeEmpty();
+        }
+    }
+}
