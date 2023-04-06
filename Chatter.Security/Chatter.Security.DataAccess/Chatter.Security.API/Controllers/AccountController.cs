79fa5ef1f8631322d2e674894c4c873c2601ae72
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-06 16:55:24 +0300

    Add Account controller.

---
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.API/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using AutoMapper;
+using Chatter.Security.API.Interfaces;
+using Chatter.Security.API.Models.Login;
+using Chatter.Security.API.Models.Register;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatter.Security.API.Controllers
+{
+    public class AccountController : BaseAPIController
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IMapper mapper, IAccountService accountService) : base(mapper)
+        {
+            _accountService = accountService;
+        }
+
+
+        [HttpPost]
+        [Route("signIn")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SignInResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> SignInAsync(SignInRequest requestModel, CancellationToken cancellationToken)
+        {
+            var result = await _accountService.SignInAsync(requestModel, cancellationToken); 
+            
+            if (!result.IsSuccessful)
+            {
+                return Unauthorized(result.Error.Message);
+            }
+
+            return Ok(result.Value);
+        }
+
+
+        [HttpPost]
+        [Route("signUp")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SignInResponse))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> SignUpAsync(SignUpRequest requestModel, CancellationToken cancellationToken)
+        {
+            var result = await _accountService.SignUpAsync(requestModel, cancellationToken);
+
+            if (!result.IsSuccessful)
+            {
+                return Unauthorized(result.Error.Message);
+            }
+
+            return Ok(result.Value);
+        }
+    }
+}
---
+++ Chatter.Security/Chatter.Security.DataAccess/Chatter.Security.API/Controllers/BaseAPIController.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Chatter.Security.Common;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chatter.Security.API.Controllers
+{
+    [ApiController]
+    [Produces("application/json")]
+    public abstract class BaseAPIController : ControllerBase
+    {
+        protected readonly IMapper _mapper;
+
+        public BaseAPIController(IMapper mapper)
+        {
+            _mapper = mapper;
+        }
+
+        protected IActionResult MapErrorResponse(ServiceResult result)
+        {
+            return result.Error.Type switch
+            {
+                ErrorType.BusinessError => BadRequest(result.Error),
+                ErrorType.NoDataError => NotFound(result.Error),
+                ErrorType.DataError => UnprocessableEntity(result.Error),
+                ErrorType.DataConflict => Conflict(result.Error),
+                _ => StatusCode(StatusCodes.Status500InternalServerError, result.Error)
+            };
+        }
+    }
+}
