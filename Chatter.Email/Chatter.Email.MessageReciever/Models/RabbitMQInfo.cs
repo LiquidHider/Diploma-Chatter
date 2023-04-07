31c54fd40f948ff42f253b4c8fed6ec272f13852
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-07 19:57:28 +0300

    Add RabbitMQ client (Chatter.Email.MessageReciever.

---
+++ Chatter.Email/Chatter.Email.MessageReciever/Models/RabbitMQInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Chatter.Email.MessageReciever.Models
+{
+    internal class RabbitMQInfo
+    {
+        public string Uri { get; set; }
+
+        public string ClientProvidedName { get; set; }
+
+        public string ExchangeName { get; set; }
+
+        public string RoutingKey { get; set; }
+
+        public string QueueName { get; set; }
+    }
+}
---
+++ Chatter.Email/Chatter.Email.MessageReciever/Program.cs
@@ -0,0 +1,50 @@
+﻿using Chatter.Email.Core.Interfaces;
+using Chatter.Email.Core.Models;
+using Chatter.Email.Core.Service;
+using Chatter.Email.MessageReciever.Models;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Text.Json;
+
+const string SenderFilename = @"..\..\..\..\Chatter.Email.MessageReciever\RabbitMQ.json";
+
+IEmailService emailService = new SmtpEmailService();
+
+var rabbitMqInfo = JsonSerializer.Deserialize<RabbitMQInfo>(File.ReadAllText(SenderFilename));
+
+ConnectionFactory factory = new();
+
+factory.Uri = new Uri(rabbitMqInfo.Uri);
+factory.ClientProvidedName = rabbitMqInfo.ClientProvidedName;
+
+IConnection connection = factory.CreateConnection();
+IModel channel = connection.CreateModel();
+
+channel.ExchangeDeclare(rabbitMqInfo.ExchangeName, ExchangeType.Direct);
+channel.QueueDeclare(rabbitMqInfo.QueueName, false, false, false);
+channel.QueueBind(rabbitMqInfo.QueueName, rabbitMqInfo.ExchangeName, rabbitMqInfo.RoutingKey, null);
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+Console.WriteLine("Ready.");
+
+consumer.Received += (sender, args) => 
+{
+    var body = args.Body.ToArray();
+
+    var messageModel = JsonSerializer.Deserialize<EmailMessageModel>(Encoding.UTF8.GetString(body));
+
+    emailService.SendEmail(messageModel);
+
+    channel.BasicAck(args.DeliveryTag, false);
+};
+
+Console.ReadLine();
+
+string consumerTag = channel.BasicConsume(rabbitMqInfo.QueueName, false, consumer);
+
+channel.Close();
+
+connection.Close();
