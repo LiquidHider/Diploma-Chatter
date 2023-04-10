b0c17dee6bad1feac1260e0c969b14112779ef09
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-11 00:06:50 +0300

    Routing fix.

--- Chatter.Web/src/Environments/environment.ts
+++ Chatter.Web/src/Environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    domainApiUrl: 'https://localhost:5040/api/',
-    securityApiUrl: 'https://localhost:8080/api/',
+    domainApiUrl: 'https://localhost:7258/',
+    securityApiUrl: 'https://localhost:7019/',
     hubUrl: 'https://localhost:5040/hubs/',
     userDefaultIcon: '/assets/userIcon.jpg'
   };
