a792125c809ea2a347497b760ef927c6fa3ed0af
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-17 21:10:41 +0300

    Add sign in validation, validation messages.

---
+++ Chatter.Web/Chatter.Web/wwwroot/js/signInValidation.js
@@ -0,0 +1,48 @@
+﻿const loginField = document.getElementById("loginField");
+
+const passwordField = document.getElementById("passwordField");
+
+const loginFieldErrorMessage = document.getElementById("loginFieldErrorMessage");
+
+const passwordFieldErrorMessage = document.getElementById("passwordFieldErrorMessage");
+
+
+const validators = [
+    {
+        field: loginField,
+        validate: () => {
+            const login = loginField.value.trim();
+            const errors = [];
+
+           if (login.length == 0) {
+                errors.push("Login field is empty.");
+           }
+
+            loginFieldErrorMessage.innerHTML = errors.length > 0 ? errors[0] : "";
+
+            return errors.length === 0;
+        },
+    },
+    {
+        field: passwordField,
+        validate: () => {
+            const password = passwordField.value.trim();
+            const errors = [];
+
+            if (password.length == 0) {
+                errors.push("Password field is empty.");
+            }
+
+            passwordFieldErrorMessage.innerHTML = errors.length > 0 ? errors[0] : "";
+
+            return errors.length === 0;
+        },
+    }
+];
+
+validators.forEach(({ field, validate }) => {
+    field.addEventListener("input", () => {
+        const allValid = validators.every(({ validate }) => validate());
+        submitButton.disabled = !allValid;
+    });
+});
