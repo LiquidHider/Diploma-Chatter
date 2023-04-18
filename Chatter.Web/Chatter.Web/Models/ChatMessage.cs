9019f647154b9b9a468d63fe1cb456c76ddbd11d
Selemetow Dmytro <syelyemyetov.dmytro.d@hneu.net>
2023-04-18 22:06:38 +0300

    Web: Add private chat messages display.

---
+++ Chatter.Web/Chatter.Web/Models/ChatMessage.cs
@@ -0,0 +1,19 @@
+﻿namespace Chatter.Web.Models
+{
+    public class ChatMessage
+    {
+        public Guid ID { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public bool IsEdited { get; set; }
+
+        public string Body { get; set; }
+
+        public DateTime Sent { get; set; }
+
+        public Guid SenderId { get; set; }
+
+        public Guid RecipientID { get; set; }
+    }
+}
---
+++ Chatter.Web/Chatter.Web/wwwroot/js/privateChat.js
@@ -0,0 +1,77 @@
+﻿const chatBody = document.getElementById("chatBody");
+
+const chatMessages = document.getElementById("chatMessages");
+
+const selectChatMessage = document.getElementById("selectChatMessage");
+
+const parsedCookie = JSON.parse(decodeCookie(getCookie("User")));
+
+const domainBaseUrl = 'https://localhost:7258/';
+
+const currentUserId = parsedCookie.userID;
+
+const userJwtToken = parsedCookie.token;
+
+chatBody.hidden = true;
+
+function openPrivateChat(member1Id, member2Id) {
+    selectChatMessage.hidden = true;
+    chatBody.hidden = false;
+    const requestModel = {
+        Member1ID: member1Id,
+        Member2ID: member2Id
+    };
+    fetch(domainBaseUrl + 'chat', {
+        method: 'POST',
+        body: JSON.stringify(requestModel),
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization' : 'Bearer ' + userJwtToken
+        }
+    })
+        .then(response => {
+            clearPrivateChat();
+            response.json().then(messages => {
+                console.log(messages)
+                for (var i = 0; i < messages.length; i++) { 
+                    displayMessage(messages[i], chatMessages);
+                }
+            })
+        })
+        .catch(error => console.log('Error opening private chat.', error));
+}
+
+function displayMessage(mes, chatBodyElement) {
+    var chatMessage = document.createElement("div");
+    var chatMessageBody = document.createElement("div");
+    var chatMessageSender = document.createElement("div");
+    var chatMessageTime = document.createElement("div");
+
+    chatMessageSender.innerHTML += isSenderCurrentUser(mes.senderId) ? "You" : mes.senderId;
+    chatMessageBody.innerHTML += mes.body;
+    chatMessageTime.innerHTML += mes.sent;
+
+    chatMessage.appendChild(chatMessageSender);
+    chatMessage.appendChild(chatMessageBody);
+    chatMessage.appendChild(chatMessageTime);
+  
+    chatBodyElement.appendChild(chatMessage);
+}
+
+function isSenderCurrentUser(message) {
+    return message.senderId === currentUserId;
+}
+
+function clearPrivateChat(){
+    chatMessages.innerHTML = "";
+}
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+function decodeCookie(cookieValue) {
+    return decodeURIComponent(cookieValue.replace(/\+/g, ' '));
+}
